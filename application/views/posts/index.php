3def4a4ee695b3f87e41efbae14f8532b032440a
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2019-05-17 16:53:31 +0200

    fix data Y-m-d

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -4,6 +4,7 @@
       <div class="card-body text-center">
         <h5 class="card-title"><?php echo $posts_item['author']; ?></h5>
         <p class="card-text"><?php echo $posts_item['message']; ?></p>
+        <p class="card-text"><?php echo date("Y-m-d", strtotime($posts_item['date'])); ?></p>
         <input type="submit" onclick="reported(<?php echo $posts_item['id']; ?>)" id="reported_btn" name="reported_btn" value="Reported" class="btn btn-warning btn-sm" />
       </div>
     </div>
